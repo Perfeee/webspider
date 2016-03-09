9a66ce414b37fb4490aadc9177954097645de3a5
Perfeee <921387206@qq.com>
2016-03-09 19:46:46 +0800

    initial project version

---
+++ urllib_test.py
@@ -0,0 +1,23 @@
+
+import urllib.request,urllib,urllib.parse
+url = 'http://www.baibai.com/'
+response = urllib.request.urlopen('http://www.baidu.com/')
+html = response.read()
+#print(html)
+
+print('helloworld')
+
+data = {}
+data['name'] = 'WHY'
+data['location'] = 'SDU'
+data['language'] = 'Python'
+
+urlvalue = urllib.parse.urlencode(data)
+print(urlvalue)
+
+#req = urllib.Request('www.baibai.com')
+try: 
+	web = urllib.request.urlopen(url)
+	print(web.read())
+except urllib.error.URLError:
+	print(urllib.error.URLError.reason)
